e3fd61c21d778b96e266dc678d17ac08e9cf799d
yinhao-mac <yinhao@jd.com>
2017-12-20 22:26:53 +0800

    实现mybatis简单查询

--- src/main/java/com/mybatis/test/mapper/RoleMapper.java
+++ src/main/java/com/mybatis/test/mapper/RoleMapper.java
@@ -4,6 +4,9 @@ import com.mybatis.test.domain.Role;
 
 /**
  * Created by yinhao on 17/12/20.
+ *
+ * 先暂时理解为mybatis执行时，会生成一个该接口的代理类
+ * 代理类会根据关联了该接口全路径的映射文件（也就是RoleMapper.xml）中定义的sql的id，执行对应方法
  */
 public interface RoleMapper {
     public Role getRole(long id);
